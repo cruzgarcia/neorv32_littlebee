b8ff8b8ebace4ac4f3fe6fc13c9945e1029ebcd9
Cruz Garcia <cruzgarcia@posteo.de>
2021-09-21 21:44:32 +0200

    Included SPI support for Flash and ePaper

--- hdl/neorv32_littlebee_top.vhd
+++ hdl/neorv32_littlebee_top.vhd
@@ -7,26 +7,52 @@ use neorv32.neorv32_package.all;
 
 entity neorv32_littlebee_top is
   generic (
-    CLOCK_FREQUENCY   : natural := 30000000; -- clock frequency of clk_i in Hz
-    MEM_INT_IMEM_SIZE : natural := 16*1024;   -- size of processor-internal instruction memory in bytes
-    MEM_INT_DMEM_SIZE : natural := 8*1024     -- size of processor-internal data memory in bytes
+    CLOCK_FREQUENCY   : natural := 30000000;  -- clock frequency of clk_i in Hz
+    MEM_INT_IMEM_SIZE : natural := 32*1024;   -- size of processor-internal instruction memory in bytes
+    MEM_INT_DMEM_SIZE : natural := 8*1024;    -- size of processor-internal data memory in bytes
+    IO_SPI_EN         : boolean := true       -- implement serial peripheral interface (SPI)?
   );
   port (
     -- Global control --
-    clk_i       : in  std_ulogic; -- global clock, rising edge
-    rstn_i      : in  std_ulogic; -- global reset, low-active, async
+    clk_i             : in  std_ulogic; -- global clock, rising edge
+    rstn_i            : in  std_ulogic; -- global reset, low-active, async
     -- GPIO --
-    gpio_o      : out std_ulogic_vector(7 downto 0); -- parallel output
+    gpio_o            : out std_ulogic_vector(7 downto 0); -- parallel output
     -- UART0 --
-    uart0_txd_o : out std_ulogic; -- UART0 send data
-    uart0_rxd_i : in  std_ulogic  -- UART0 receive data
+    uart0_txd_o       : out std_ulogic; -- UART0 send data
+    uart0_rxd_i       : in  std_ulogic; -- UART0 receive data
+    -- SPI - ePaper display
+    epaper_busy_i     : in  std_ulogic;
+    epaper_rst_o      : out std_ulogic;
+    epaper_dc_o       : out std_ulogic;
+    epaper_spi_sck_o  : out std_ulogic;       -- SPI serial clock
+    epaper_spi_sdo_o  : out std_ulogic;       -- controller data out, peripheral data in
+    epaper_spi_sdi_i  : in  std_ulogic:= 'U'; -- controller data in, peripheral data out
+    epaper_spi_csn_o  : out std_ulogic;       -- chip select
+    -- Serial Flash
+    flash_cs_o        : out std_logic;
+    flash_clk_o       : out std_logic;
+    flash_so          : out std_logic;
+    flash_si          : in  std_logic;
+    flash_i02         : out std_logic;
+    flash_i03         : out std_logic
   );
 end entity;
 
 architecture neorv32_littlebee_top_rtl of neorv32_littlebee_top is
 
+  -- SPI
+  constant c_spi_csn_flash_index    : integer := 0;
+  constant c_spi_csn_epaper_index   : integer := 1;
+
   signal con_gpio_o   : std_ulogic_vector(63 downto 0);
+  signal con_gpio_i   : std_ulogic_vector(63 downto 0);
   signal clock_30mhz  : std_logic;
+  -- SPI 
+  signal spi_sck      : std_ulogic;
+  signal spi_sdo      : std_ulogic;
+  signal spi_sdi      : std_ulogic;
+  signal spi_csn      : std_ulogic_vector(7 downto 0);
 
 begin
 
@@ -56,21 +82,53 @@ begin
     -- Processor peripherals --
     IO_GPIO_EN                   => true,              -- implement general purpose input/output port unit (GPIO)?
     IO_MTIME_EN                  => true,              -- implement machine system timer (MTIME)?
-    IO_UART0_EN                  => true               -- implement primary universal asynchronous receiver/transmitter (UART0)?
+    IO_UART0_EN                  => true,              -- implement primary universal asynchronous receiver/transmitter (UART0)?
+    -- Processor peripherals --
+    IO_SPI_EN                    => IO_SPI_EN
   )
   port map (
     -- Global control --
-    clk_i       => clock_30mhz, -- global clock, rising edge
+    clk_i       => clock_30mhz,
     rstn_i      => rstn_i,      -- global reset, low-active, async
     -- GPIO (available if IO_GPIO_EN = true) --
-    gpio_o      => con_gpio_o,  -- parallel output
+    gpio_o      => con_gpio_o,
+    gpio_i      => con_gpio_i,
     -- primary UART0 (available if IO_UART0_EN = true) --
-    uart0_txd_o => uart0_txd_o, -- UART0 send data
-    uart0_rxd_i => uart0_rxd_i  -- UART0 receive data
+    uart0_txd_o => uart0_txd_o,
+    uart0_rxd_i => uart0_rxd_i,
+    -- SPI (available if IO_SPI_EN = true) --
+    spi_sck_o   => spi_sck,
+    spi_sdo_o   => spi_sdo,
+    spi_sdi_i   => spi_sdi,
+    spi_csn_o   => spi_csn
   );
 
-  -- GPIO output --
+  -- GPIO output
   gpio_o <= con_gpio_o(7 downto 0);
 
+  -- e-Paper display SPI
+  epaper_rst_o      <= con_gpio_o(8);
+  epaper_dc_o       <= con_gpio_o(9);
+  epaper_spi_sck_o  <= spi_sck;
+  epaper_spi_sdo_o  <= spi_sdo;
+  epaper_spi_csn_o  <= spi_csn(c_spi_csn_epaper_index);
+  con_gpio_i(10)    <= epaper_busy_i;
+
+  -- Flash
+  flash_cs_o        <= spi_csn(c_spi_csn_epaper_index);
+  flash_clk_o       <= spi_sck;
+  flash_so          <= spi_sdo;
+  flash_i02         <= 'Z';
+  flash_i03         <= 'Z';
+
+
+  proc_flash_din_mux : process(all)
+  begin
+    case spi_csn is
+      when b"11111110" => spi_sdi <= flash_si;
+      when b"11111101" => spi_sdi <= epaper_spi_sdi_i;
+      when others => spi_sdi <= '1';
+    end case;
+  end process;
 
 end architecture;
