6e51146a3eb893e0b6763cb0fecbf30704f54a76
Cruz Garcia <cruzgarcia@posteo.de>
2021-09-25 22:59:50 +0200

    Included initial Intel FPGA project sketch for MKR Vidor 4000. The Quartus project is still missing contraining and correct assignations, however it is useful to validate only VHDL 93 support in Quartus Lite version and max frequency of about 80 MHz

---
+++ hdl/mkr_vidor_4000_pkg.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package global_configuration is
+  
+  constant c_FPGA_MANUFACTURER : STRING  := "INTEL";
+
+end package global_configuration;
+ 
+package body global_configuration is
+ 
+end package body global_configuration;
